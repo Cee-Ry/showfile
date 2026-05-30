a56fd7cab7cf94c7f4b8dfc5386f5dc33e02a0de
Cee-Ry <eduaveceasar@gmail.com>
2026-05-30 12:13:05 +0800

    just save it

--- main.cpp
+++ main.cpp
@@ -2,9 +2,40 @@
 #include <vector>
 #include <fstream>
 
-int main(int argc, char *argv[]) {
-    std::vector<std::string> file_content;
+using vecStr = std::vector<std::string>;
+using read = std::ifstream;
 
+void readfile(vecStr &file, vecStr &content);
+
+int main(int argc, char *argv[]) {
+    vecStr file_contents;
+    vecStr file_names;
+
+    if (argc == 1) {
+        printf("[FILENAME] or/and [OPTION] must be add\n");
+        printf("Try 'show --help' or 'show -h' for more information\n");
+        return 1;
+    } else {
+        for (int i = 1; i < argc; i++) {
+            file_names.push_back(argv[i]);
+        }
+    }
+
+    readfile(file_names, file_contents);
 
     return 0;
+}
+
+void readfile(vecStr &file, vecStr &content) {
+    for (int i = 0; i < file.size(); i++) {
+        std::ifstream name(file.at(i));
+
+        if (!name.is_open()) {
+            printf("show: %s: No such File or Option\n", file.at(i).c_str());
+            printf("Try 'show --help' or 'show -h' for more information\n");
+            exit(1);
+        } else {
+            while (std::getline(name, content));
+        }
+    }
 }
